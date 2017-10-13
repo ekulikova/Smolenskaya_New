08e1807a2d727257be8ceaf0954c1051afccfaea
ekulikova <ek270285@gmail.com>
2017-10-13 12:09:35 -0500

    add code for menu
    first iteration

--- header.php
+++ header.php
@@ -20,4 +20,39 @@
 			<div class="name">
 				<img class="name_img" src='<?php echo get_template_directory_uri(); ?>/img/name.png' />
 			</div>
+			
+			<nav>
+				<ul>
+					<li><a href="#">Really Tall Menu</a>
+						<ul>
+							<li><a href="#">Lorem</a>
+								<ul>
+									<li><a href="#">Resets</a></li>
+									<li><a href="#">Grids</a></li>
+									<li><a href="#">Frameworks</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Ipsum</a></li>
+							<li><a href="#">Dolor</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Kinda Tall Menu</a>
+						<ul>
+							<li><a href="#">Lorem</a></li>
+							<li><a href="#">Ipsum</a></li>
+							<li><a href="#">Dolor</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Average Menu</a>
+						<ul>
+							<li><a href="#">Lorem</a></li>
+							<li><a href="#">Ipsum</a></li>
+							<li><a href="#">Dolor</a></li>
+						</ul>
+					</li>
+					<li><a href="#">No Menu</a>
+					</li>
+				</ul>
+			</nav> 
+
 		</header>
